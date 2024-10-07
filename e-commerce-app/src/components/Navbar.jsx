0476511161e326f8618a66681beb9892c4db195d
SyedFazal110 <syedfazal808@gmail.com>
2024-10-07 10:31:56 +0500

    Work Updarted

--- e-commerce-app/src/components/Navbar.jsx
+++ e-commerce-app/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <>
-            <div class=" flex justify-between items-center navbar bg-blue-400">
+            <div class="flex justify-between items-center p-5 navbar bg-blue-400">
                 <div class="navbar-start">
                     <div class="dropdown">
                         <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
@@ -24,33 +24,12 @@ const Navbar = () => {
                     </div>
                     <a class="btn btn-ghost text-xl">daisyUI</a>
                 </div>
-                <div class="navbar-center hidden lg:flex">
-                    <ul class="menu menu-horizontal px-1 gap-10 text-lg font-semibold">
+                <div class="navbar-center lg:flex">
+                    <ul class="flex menu menu-horizontal px-1 gap-10 text-lg font-bold">
                         <li><Link to="">Home</Link></li>
                         <li><Link to="about">About</Link></li>
                         <li><Link to="contact">Contact</Link></li>
                         <li><Link to="services">Services</Link></li>
-                    </ul>
-                </div>
-                <div class="dropdown dropdown-end">
-                    <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-                        <div class="w-10 rounded-full">
-                            <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                        </div>
-                    </div>
-                    <ul
-                        tabindex="0"
-                        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li>
-                            <a class="justify-between">
-                                Profile
-                                <span class="badge">New</span>
-                            </a>
-                        </li>
-                        <li><a>Settings</a></li>
-                        <li><a>Logout</a></li>
                     </ul>
                 </div>
             </div>
