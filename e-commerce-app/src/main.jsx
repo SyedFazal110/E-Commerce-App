71e674791b8bcfb05bc088d5919e4b22514b8588
SyedFazal110 <syedfazal808@gmail.com>
2024-10-07 09:54:06 +0500

    Work completed

---
+++ e-commerce-app/src/main.jsx
@@ -0,0 +1,47 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
+import Services from './pages/Services.jsx';
+import SingleProduct from './pages/SingleProduct.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      },
+      {
+        path: "services",
+        element: <Services />
+      },
+      {
+        path: "singleproduct/:id",
+        element: <SingleProduct />
+      },
+      {
+        path: "*",
+        element: <h1>Not Found!!</h1>
+      },
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router = {router}></RouterProvider>
+)
